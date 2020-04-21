c96572e31466a04dddeaecacea390428abd7a14a
Matthew Martin <martim01@outlook.com>
2020-04-21 12:13:47 +0100

    Adding missing mp3streamer files

---
+++ plugins/MP3Streamer/ondemandmp3servermediasubsession.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <wx/event.h>
+#include "mp3encoder.h"
+#include "ondemandpamsubsession.h"
+class PamUsageEnvironment;
+
+class OnDemandMP3MediaSubsession: public OnDemandPamSubsession
+{
+    public:
+        static OnDemandMP3MediaSubsession* createNew(wxEvtHandler* pHandler, PamUsageEnvironment& env, MP3Encoder& encoder,  portNumBits initialPortNum = 6970);
+    protected: // we're a virtual base class
+        OnDemandMP3MediaSubsession(wxEvtHandler* pHandler, PamUsageEnvironment& env, MP3Encoder& encoder,  portNumBits initialPortNum  = 6970);
+        virtual ~OnDemandMP3MediaSubsession();
+
+        // new virtual functions, defined by all subclasses
+        FramedSource* createNewStreamSource(unsigned clientSessionId, unsigned& estBitrate) override;
+        // "estBitrate" is the stream's estimated bitrate, in kbps
+        RTPSink* createNewRTPSink(Groupsock* rtpGroupsock, unsigned char rtpPayloadTypeIfDynamic, FramedSource* inputSource) override;
+
+    private:
+        MP3Encoder& m_Encoder;
+};
+
+
---
+++ plugins/MP3Streamer/testMP3Streamer.h
@@ -0,0 +1,6 @@
+#pragma once
+class MP3Encoder;
+
+extern void stream(MP3Encoder& encoder);
+
+
