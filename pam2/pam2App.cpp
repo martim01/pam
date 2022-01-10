6d92459b0a9b91569b15674e07a3a02be5a1ef13
Matthew Martin <martim01@outlook.com>
2022-01-10 16:04:58 +0000

    improvement: use user config directory for pam2.ini

--- pam2/pam2App.cpp
+++ pam2/pam2App.cpp
@@ -57,7 +57,7 @@ bool pam2App::OnInit()
 
 
     //#ifdef __WXGNU__
-    Settings::Get().ReadSettings(wxString::Format(wxT("%s/pam/pam2.ini"), wxStandardPaths::Get().GetDocumentsDir().c_str()));
+    Settings::Get().ReadSettings(wxString::Format(wxT("%s/pam/pam2.ini"), wxStandardPaths::Get().GetUserConfigDir().c_str()));
     Settings::Get().RemoveKey(wxT("AoIP"), wxT("NMOS_IS-04"));
     Settings::Get().Write("Startup", "Starting",1);
 
