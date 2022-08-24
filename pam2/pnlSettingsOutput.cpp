5c4362c8f32e90cee7f2f963219e985c4ad51cef
Matthew Martin <martim01@outlook.com>
2022-08-24 15:48:25 +0100

    fix RTCP enable button

--- pam2/pnlSettingsOutput.cpp
+++ pam2/pnlSettingsOutput.cpp
@@ -283,7 +283,7 @@ pnlSettingsOutput::pnlSettingsOutput(wxWindow* parent,wxWindowID id,const wxPoin
 
     m_pbtnDNS->ConnectToSetting("Server","DNS-SD", true);
     m_pbtnSAP->ConnectToSetting("Server","SAP", false);
-    m_pbtnRTCP->ConnectToSetting("Server","RCTCP",true);
+    m_pbtnRTCP->ConnectToSetting("Server","RTCP",true);
 
     m_pbtnRtpMap->ConnectToSetting("Server","RTPMap", "96");
     m_pbtnRtpMap->SetPopup({"96","97","98","99","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122","123","124","125","126","127"});
